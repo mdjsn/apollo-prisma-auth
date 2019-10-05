b359a27cb5b9309c79f239ea778b4c84d42c4481
aryaminus <sunim.54@gmail.com>
2019-10-05 13:43:34 +0545

    Deploy Prisma for new values

--- src/generated/prisma-client/prisma-schema.ts
+++ src/generated/prisma-client/prisma-schema.ts
@@ -51,7 +51,9 @@ type Subscription {
 
 type User {
   id: ID!
+  email: String!
   name: String!
+  password: String!
 }
 
 type UserConnection {
@@ -62,7 +64,9 @@ type UserConnection {
 
 input UserCreateInput {
   id: ID
+  email: String!
   name: String!
+  password: String!
 }
 
 type UserEdge {
@@ -73,13 +77,19 @@ type UserEdge {
 enum UserOrderByInput {
   id_ASC
   id_DESC
+  email_ASC
+  email_DESC
   name_ASC
   name_DESC
+  password_ASC
+  password_DESC
 }
 
 type UserPreviousValues {
   id: ID!
+  email: String!
   name: String!
+  password: String!
 }
 
 type UserSubscriptionPayload {
@@ -101,11 +111,15 @@ input UserSubscriptionWhereInput {
 }
 
 input UserUpdateInput {
+  email: String
   name: String
+  password: String
 }
 
 input UserUpdateManyMutationInput {
+  email: String
   name: String
+  password: String
 }
 
 input UserWhereInput {
@@ -123,6 +137,20 @@ input UserWhereInput {
   id_not_starts_with: ID
   id_ends_with: ID
   id_not_ends_with: ID
+  email: String
+  email_not: String
+  email_in: [String!]
+  email_not_in: [String!]
+  email_lt: String
+  email_lte: String
+  email_gt: String
+  email_gte: String
+  email_contains: String
+  email_not_contains: String
+  email_starts_with: String
+  email_not_starts_with: String
+  email_ends_with: String
+  email_not_ends_with: String
   name: String
   name_not: String
   name_in: [String!]
@@ -137,6 +165,20 @@ input UserWhereInput {
   name_not_starts_with: String
   name_ends_with: String
   name_not_ends_with: String
+  password: String
+  password_not: String
+  password_in: [String!]
+  password_not_in: [String!]
+  password_lt: String
+  password_lte: String
+  password_gt: String
+  password_gte: String
+  password_contains: String
+  password_not_contains: String
+  password_starts_with: String
+  password_not_starts_with: String
+  password_ends_with: String
+  password_not_ends_with: String
   AND: [UserWhereInput!]
   OR: [UserWhereInput!]
   NOT: [UserWhereInput!]
